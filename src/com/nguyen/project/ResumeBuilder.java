660168db57508b4a5c06e7c5185b87b05bd8eb7f
Diem Nguyen <t.nguyendp@gmail.com>
2018-06-20 14:55:14 -0400

    Prompt user for input

--- src/com/nguyen/project/ResumeBuilder.java
+++ src/com/nguyen/project/ResumeBuilder.java
@@ -1,21 +1,140 @@
-package com.nguyen.project;
+package com.nguyen.week3.project;
+
+import com.nguyen.project.Education;
+import com.nguyen.project.Experience;
+import com.nguyen.project.Resume;
+import com.nguyen.project.Skill;
+
+import java.util.Scanner;
 
 public class ResumeBuilder {
 
-    static Resume myResume;
-    static Education anEducation;
-    static Experience anExperience;
-    static Skill aSkill;
+    private static Resume myResume;
+    private static Education anEducation;
+    private static Experience anExperience;
+    private static Skill aSkill;
 
+    private static Scanner sc = new Scanner(System.in);
+
+    private static String input = "";
     private static String display = "";
 
     public static void main(String[] args) {
-        populateClasses();
+        //populateClasses();
+
+        askPersonalInfo();
         populateUserInfo();
+
+        askEducations();
+        display+="EDUCATIONS \n";
         populateEducations();
+
+        askExperiences();
+        display+="EXPERIENCES \n";
         populateExperiences();
+
+        askSkills();
+        display+="SKILLS \n";
         populateSkills();
-        System.out.println(displayResume());
+
+        println(displayResume());
+    }
+
+    public static void askPersonalInfo() {
+        myResume = new Resume();
+        print("Enter your first name: ");
+        input = sc.nextLine();
+        myResume.setFirstName(input);
+
+        print("Enter your last name: ");
+        input = sc.nextLine();
+        myResume.setLastName(input);
+
+        print("Enter your email address: ");
+        input = sc.nextLine();
+        myResume.setEmail(input);
+
+        print("Enter your phone number: ");
+        input = sc.nextLine();
+        myResume.setLastName(input);
+        input = "";//clear input
+    }
+
+    public static void askEducations() {
+        println("*****  Enter your educations...  *****");
+        while (!(input.equalsIgnoreCase("n"))){
+            anEducation = new Education();
+            print("Enter your degree type: ");
+            input = sc.nextLine();
+            anEducation.setDegreeType(input);
+
+            print("Enter your degree name: ");
+            input = sc.nextLine();
+            anEducation.setDegreeName(input);
+
+            print("Enter your university name: ");
+            input = sc.nextLine();
+            anEducation.setUniversityName(input);
+
+            print("Enter your university's city: ");
+            input = sc.nextLine();
+            anEducation.setCity(input);
+
+            print("Enter your university's state: ");
+            input = sc.nextLine();
+            anEducation.setState(input);
+
+            print("Do you want to enter another (y/n)? ");
+            input = sc.next();
+            myResume.addEducation(anEducation);
+        }
+        input = "";
+    }
+
+    public static void askExperiences() {
+        println("*****  Enter your experiences...  *****");
+        while (!(input.equalsIgnoreCase("n"))){
+            anExperience = new Experience();
+            print("Enter the company name: ");
+            input = sc.nextLine();
+            anExperience.setCompanyName(input);
+
+            print("Enter the job title: ");
+            input = sc.nextLine();
+            anExperience.setJobTitle(input);
+
+            print("Enter the description: ");
+            input = sc.nextLine();
+            anExperience.setDescription(input);
+
+            print("Enter the duration at this company (months): ");
+            int durationInput = sc.nextInt();
+            anExperience.setWorkDuration(durationInput);
+
+            print("Do you want to enter another (y/n)? ");
+            input = sc.nextLine();
+            myResume.addExperience(anExperience);
+        }
+        input = "";
+    }
+
+    public static void askSkills() {
+        println("*****  Enter your skills...   *****");
+        while (!(input.equalsIgnoreCase("n"))){
+            aSkill = new Skill();
+            print("Enter the skill name: ");
+            input = sc.nextLine();
+            aSkill.setSkillName(input);
+
+            print("Enter the level of proficiency: ");
+            input = sc.nextLine();
+            aSkill.setProficiency(input);
+
+            print("Do you want to enter another (y/n)? ");
+            input = sc.nextLine();
+            myResume.addSkill(aSkill);
+        }
+        input = "";
     }
 
     public static void populateClasses() {
@@ -28,8 +147,6 @@ public class ResumeBuilder {
         myResume.addExperience(anExperience);
         myResume.addSkill(aSkill);
 
-        //System.out.println("Created a default profile...");
-
     }
 
     public static void populateUserInfo() {
@@ -38,7 +155,6 @@ public class ResumeBuilder {
     }
 
     public static void populateEducations() {
-        display+="EDUCATION \n";
         for (Education thisEducation : myResume.getEducations()) {
             display+=thisEducation.getUniversityName()+"\t\t"+thisEducation.getCity()+", "+thisEducation.getState()+"\n"
                     +thisEducation.getDegreeType()+" "+thisEducation.getDegreeName()+"\n\n";
@@ -46,7 +162,6 @@ public class ResumeBuilder {
     }
 
     public static void populateExperiences() {
-        display+="EXPERIENCES \n";
         for (Experience thisExperience : myResume.getExperiences()) {
             display+=thisExperience.getJobTitle()+"\t\t"+thisExperience.getCompanyName()+" "+thisExperience.getWorkDuration()+" years"+"\n"
                     +thisExperience.getDescription()+"\n\n";
@@ -54,7 +169,6 @@ public class ResumeBuilder {
     }
 
     public static void populateSkills() {
-        display+="SKILLS \n";
         for (Skill thisSkill : myResume.getSkills()) {
             display+=thisSkill.getSkillName()+"\t\t"+thisSkill.getProficiency()+"\n\n";
         }
@@ -62,6 +176,13 @@ public class ResumeBuilder {
 
     public static String displayResume() {
         return display;
+    }
+
+    private static void println(String output){
+        System.out.println(output);
+    }
+    private static void print(String output) {
+        System.out.print(output);
     }
 
 }
